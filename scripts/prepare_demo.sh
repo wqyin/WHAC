950f18dffe1d34c235ba9b2a8667ad7693589b1b
yinwanqi <wanqi.yin5@gmail.com>
2025-02-20 20:47:24 +0800

    add videos for demo

---
+++ scripts/prepare_demo.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+mkdir ./demo
+wget https://drive.google.com/file/d/1Owt3ylHtgl-LKbM2d3Gg6EqX6YBXQauK/view?usp=sharing -O ./demo/dance_demo.mp4
+wget https://drive.google.com/file/d/1ncw6E_CWMcEpAzo_SdrU6oGGtIvUgqHN/view?usp=sharing -O ./demo/skateboard_demo.mp4
